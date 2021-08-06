7384bcd4b182664ab00277d242bf60ee7a9273e4
BETÜL ALBAYRAK <52135580+BetulAlbayrak1999@users.noreply.github.com>
2021-08-06 11:53:06 +0100

    Operators

---
+++ Operator/Program.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace C__pratic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 7;
+            int y = 6;
+            bool ifTure = x ==y;
+            if(x > y){
+                Console.WriteLine(" x > y ");
+            }
+            else if(x > y){
+                Console.WriteLine("x > y");
+            }
+            else {
+                Console.WriteLine("x = y");
+            }
+            
+
+        }
+    }
+}
