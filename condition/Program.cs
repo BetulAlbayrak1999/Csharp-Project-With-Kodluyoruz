87b2159ef4c81a9691fee7cddebdfacd61dc0538
BETÜL ALBAYRAK <52135580+BetulAlbayrak1999@users.noreply.github.com>
2021-08-07 09:52:23 +0100

    Condition

---
+++ condition/Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace C__pratic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 7;
+            int y = 6;
+            string result = x > y ? "x>y" : "x<y";
+            Console.WriteLine(result);
+
+        }
+    }
+}
